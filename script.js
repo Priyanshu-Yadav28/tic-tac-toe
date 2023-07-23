b2bd2712be317144e5a1413ee9a13105f93250b8
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2023-07-23 14:25:29 +0530

    js added

--- script.js
+++ script.js
@@ -32,137 +32,112 @@ const winningPositions = [
 function initGame() {
     currentPlayer = "X";
     gameGrid = ["", "", "", "", "", "", "", "", ""];
-    newGameBtn.classList.remove("active");
+
+    // UI par empty bhi karna padega when we click on new game button 
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        // one moer thing left -> intitialise box with css properties again 
+        box.classList = `box box${index + 1}`; 
+    });
+
+    newGameBtn.classList.remove("active"); 
     gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
 
 initGame();
 
-function handleClick(index) {
-    
+function swapTurn() {
+    if(currentPlayer === "X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
+    }
+    // UI info
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
+
+
+function checkGameOver() {
+    let answer = "";
+
+    winningPositions.forEach((position) => {
+        // all 3 boxes should be non-empty and exactly same in value
+        if ((gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "" ) && (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+
+            // chek if winner is X
+            if (gameGrid[position[0]] === "X") {
+                answer = "X";
+            } else {
+                answer = "O";
+            }
+
+            // disable pointer event
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            });
+
+            // now we know X/O is a winner 
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+}
+
+    });
+    
+    // it means we have a winner 
+    if(answer !== "" ) {
+        gameInfo.innerText = `Winner Player - ${answer}`;
+        newGameBtn.classList.add("active");
+        return;
+    } 
+
+    //let's check when there is tie : 
+    let fillCount = 0;
+
+    gameGrid.forEach((box) => {
+        if(box !== "") {
+            fillCount++;
+        }
+    });
+
+    // board is filled, game is TIE
+
+    if(fillCount === 9) {
+        gameInfo.innerText = "Match Tied !";
+        newGameBtn.classList.add("active");
+    }
+     
+
+} 
+
+
+
+function handleClick(index) {
+    // newGameBtn.classList.add("active");
+     if(gameGrid[index] == "") {
+        boxes[index].innerText = currentPlayer; // changes the UI
+        gameGrid[index] = currentPlayer; // changes the gameGrid array 
+        boxes[index].style.pointerEvents = "none";
+        // swap the turn 
+        swapTurn();
+        // check koi jeet to nahi gaya
+        checkGameOver();
+     }
+};
 
 boxes.forEach((box, index) => {
     box.addEventListener("click", () => {
         handleClick(index);
     })
-}) 
+});
+
+newGameBtn.addEventListener("click", initGame);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let count = 0;
-// box1.addEventListener("click", function() {
-    
-//     if (count % 2 == 0) {
-//         box1.innerHTML = "X";
-//     }
-//     else{
-//         box1.innerHTML = "O";
-//     }
-//     count ++;
-// }, {once: true});
-
-// box2.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box2.innerHTML = "X";
-//     }
-//     else{
-//         box2.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box3.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box3.innerHTML = "X";
-//     }
-//     else{
-//         box3.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box4.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box4.innerHTML = "X";
-//     }
-//     else{
-//         box4.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box5.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box5.innerHTML = "X";
-//     }
-//     else{
-//         box5.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box6.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box6.innerHTML = "X";
-//     }
-//     else{
-//         box6.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box7.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box7.innerHTML = "X";
-//     }
-//     else{
-//         box7.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box8.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box8.innerHTML = "X";
-//     }
-//     else{
-//         box8.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
-
-// box9.addEventListener("click", function() {
-//     if (count % 2 == 0) {
-//         box9.innerHTML = "X";
-//     }
-//     else{
-//         box9.innerHTML = "O";
-//     }
-//     count ++;
-
-// }, {once: true});
 
 
 
